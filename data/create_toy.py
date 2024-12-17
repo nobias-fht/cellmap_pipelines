7a82c1b0c269ad63e7a7bbb56581398e167a7542
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2024-12-17 22:15:55 +0100

    Update toy example for multichannel

--- data/create_toy.py
+++ data/create_toy.py
@@ -8,11 +8,10 @@ import json
 # we create random spheres in 3D in two different classes
 shape = (512, 512, 512)
 array = np.zeros(shape, dtype=np.uint8)
-labels = np.zeros(shape, dtype=np.uint8)
+labels = np.zeros((2,) + shape, dtype=np.uint8)
 N_sphere = 100
 radii = [20, 60]
 p = [0.8, 0.2]
-label = [1, 2]
 intensity = [150, 80]
 
 metadata = {
@@ -31,7 +30,7 @@ for _ in tqdm(range(N_sphere)):
     x, y, z = np.ogrid[:shape[0], :shape[1], :shape[2]]
     mask = (x - center[0])**2 + (y - center[1])**2 + (z - center[2])**2 < radius**2
     array[mask] = intensity[which_class]
-    labels[mask] = label[which_class]
+    labels[which_class, mask] = 1
 
 
 # add poisson noise to array and enforce uint8
@@ -46,7 +45,7 @@ for i in range(0, shape[0], crop_shape[0]):
     for j in range(0, shape[1], crop_shape[1]):
         for k in range(0, shape[2], crop_shape[2]):
             crops.append(array[i:i+crop_shape[0], j:j+crop_shape[1], k:k+crop_shape[2]])
-            crop_labels.append(labels[i:i+crop_shape[0], j:j+crop_shape[1], k:k+crop_shape[2]])
+            crop_labels.append(labels[:, i:i+crop_shape[0], j:j+crop_shape[1], k:k+crop_shape[2]])
 
             # update crop coordinate
             metadata["crops"][f"crop_{len(crops)-1:02d}"] = {
